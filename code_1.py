ba031cfdf3fa0044a13971a689b37c362dc75ea6
yu-ololo <43878640+yu-ololo@users.noreply.github.com>
2023-05-08 01:48:11 +0900

    wip

--- code_1.py
+++ code_1.py
@@ -1,14 +1,13 @@
-
 def repeatedSubstringPattern(s: str) -> bool:
     s_len = len(list(s))
-    for i in range(1,(s_len//2)+1):
-        if s_len != 1 and s[:i]*(s_len//i) == s:
+    for i in range(1, (s_len // 2) + 1):
+        if s_len != 1 and s[:i] * (s_len // i) == s:
             return True
     else:
         return False
+
 
 if __name__ == "__main__":
     # Repeated Substring Pattern
     s = "abcabc"
     print(repeatedSubstringPattern(s))
-
--- day1.py
+++ day1.py
@@ -14,4 +14,44 @@ while right - left > 1:
     else:
         right = mid
 
+# %% bad version
+m = 5
+bad = 4
+
+
+def isBadVersion(version):
+    return version >= bad
+
+
+def firstBadVersion(n):
+    left = -1
+    right = n
+    while right - left > 1:
+        mid = (left + right) // 2
+        if isBadVersion(mid):
+            right = mid
+        else:
+            left = mid
+    return right
+
+
+print(firstBadVersion(m))
 # %%
+n = 2021
+
+
+def numSquares(n):
+    n = int(n)
+    dp = [0] * (n + 1)
+    print(dp)
+    for i in range(1, n + 1):
+        dp[i] = i
+        for j in range(1, i):
+            if j * j > i:
+                break
+            dp[i] = min(dp[i], dp[i - j * j] + 1)
+        print(dp)
+    return dp[n]
+
+
+print(numSquares(n))
